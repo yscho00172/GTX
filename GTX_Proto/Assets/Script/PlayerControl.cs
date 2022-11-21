3826c1949e3bc8d89923e0667134e8271593a895
yscho00172 <yscho00172@gmail.com>
2022-11-21 17:55:56 +0900

    Change Default Setting
    Changed default movement setting

--- GTX_Proto/Assets/Script/PlayerControl.cs
+++ GTX_Proto/Assets/Script/PlayerControl.cs
@@ -3,9 +3,9 @@ using UnityEngine.Events;
 
 public class PlayerControl : MonoBehaviour
 {
-    [SerializeField] private float m_JumpForce = 400f;                          // Amount of force added when the player jumps.
+    [SerializeField] private float m_JumpForce = 800f;                          // Amount of force added when the player jumps.
     [Range(0, .3f)][SerializeField] private float m_MovementSmoothing = .05f;   // How much to smooth out the movement
-    [SerializeField] private bool m_AirControl = false;                         // Whether or not a player can steer while jumping;
+    [SerializeField] private bool m_AirControl = true;                         // Whether or not a player can steer while jumping;
     [SerializeField] private LayerMask m_WhatIsGround;                          // A mask determining what is ground to the character
     [SerializeField] private Transform m_GroundCheck;                           // A position marking where to check if the player is grounded.
     [SerializeField] private Transform m_CeilingCheck;                          // A position marking where to check for ceilings
